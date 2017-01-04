2f550c252a3e57cec7785a53ed199425f84e7255
Sanjay Kumar Yadav <sanjay.yadav@appster.com.au>
2017-01-04 10:18:57 +0530

    Reference Table Cell created

---
+++ DentaMatch/Views/TableView Cells/Profile/ReferenceTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  ReferenceTableCell.swift
+//  DentaMatch
+//
+//  Created by Sanjay Kumar Yadav on 03/01/17.
+//  Copyright © 2017 Appster. All rights reserved.
+//
+
+import UIKit
+
+class ReferenceTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
