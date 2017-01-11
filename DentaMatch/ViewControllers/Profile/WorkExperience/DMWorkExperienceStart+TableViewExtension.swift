c97bb8a43fe319a585fcfd7e6c89cd124cb0acca
Sanjay Kumar Yadav <sanjay.yadav@appster.com.au>
2017-01-11 12:53:02 +0530

    DevBox issue bug Fixes

--- DentaMatch/ViewControllers/Profile/WorkExperience/DMWorkExperienceStart+TableViewExtension.swift
+++ DentaMatch/ViewControllers/Profile/WorkExperience/DMWorkExperienceStart+TableViewExtension.swift
@@ -14,44 +14,47 @@ extension DMWorkExperienceStart
     
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
+        return 1
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if section == 0
-        {
-            return 1
-        }else{
-            return 3
-        }
+        return 5
+//        if section == 0
+//        {
+//            return 1
+//        }else{
+//            return 3
+//        }
     }
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView?
-    {
-        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.bounds.size.width, height: 45))
-        let headerLabel = UILabel(frame: headerView.frame)
-        headerLabel.frame.origin.x = 20
-        headerLabel.backgroundColor = UIColor.clear
-        headerLabel.font = UIFont.fontMedium(fontSize: 14)
-        headerView.addSubview(headerLabel)
-        headerView.backgroundColor = UIColor(red: 248.0/255.0, green: 248.0/255.0, blue: 248.0/255.0, alpha: 1.0)
-        if section == 0
-        {
-            headerLabel.text = ""
-        }else{
-            headerLabel.text = "Work Experience"
-        }
-        
-        return headerView
-    }
+//    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView?
+//    {
+//        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.bounds.size.width, height: 45))
+//        let headerLabel = UILabel(frame: headerView.frame)
+//        headerLabel.frame.origin.x = 20
+//        headerLabel.backgroundColor = UIColor.clear
+//        headerLabel.font = UIFont.fontMedium(fontSize: 14)
+//        headerView.addSubview(headerLabel)
+//        headerView.backgroundColor = UIColor(red: 248.0/255.0, green: 248.0/255.0, blue: 248.0/255.0, alpha: 1.0)
+//        if section == 0
+//        {
+//            headerLabel.text = ""
+//        }else{
+//            headerLabel.text = "Work Experience"
+//        }
+//        
+//        return headerView
+//    }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         
-        if indexPath.section == 0
-        {
-            return 226
-            
-        }else {
+        switch indexPath.row {
+        case 0:
+            return 213
+        case 1:
+            return 45
+        case 2,3,4:
             return 75
-            
+        default:
+            return 0
         }
         
     }
@@ -61,54 +64,68 @@ extension DMWorkExperienceStart
         {
             return 0
         }
-        return 45
+        return 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        if  indexPath.section == 0 {
-            
+        
+        switch indexPath.row {
+        case 0:
             //PhotoNameCell
             let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoNameCell") as! PhotoNameCell
+            cell.nameLabel.text = "Where do you work?"
+            cell.jobTitleLabel.text = "Relevant work experience strengthens your profile"
+            cell.photoButton.progressBar.setProgress(profileProgress, animated: true)
+
             cell.selectionStyle = .none
             
             return cell
 
+        case 1:
+            //PhotoNameCell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "SectionHeadingTableCell") as! SectionHeadingTableCell
+            cell.selectionStyle = .none
+            cell.headingLabel.text = "Work Experience"
             
-        }else{
+            return cell
+
+        case 2,3,4:
             let cell = tableView.dequeueReusableCell(withIdentifier: "AnimatedPHTableCell") as! AnimatedPHTableCell
             cell.selectionStyle = .none
-            cell.commonTextFiled.tag = indexPath.row
-            cell.commonTextFiled.text = self.experienceArray[indexPath.row] as? String
+//            cell.commonTextFiled.text = self.experienceArray[indexPath.row-2] as? String
             switch indexPath.row {
-            case 0:
+            case 2:
                 cell.commonTextFiled.placeholder = FieldType.CurrentJobTitle.description
                 
                 let pickerView = JobSelectionPickerView.loadJobSelectionView(withJobTitles: jobTitles)
                 cell.commonTextFiled.inputView = pickerView
                 pickerView.delegate = self
+                cell.commonTextFiled.tag = 0
                 pickerView.pickerView.reloadAllComponents()
                 pickerView.backgroundColor = UIColor.white
-
-            case 1:
-                cell.commonTextFiled.placeholder = FieldType.YearOfExperience.description
-                let yearViewObj = ExperiencePickerView.loadExperiencePickerView(withText: self.experienceArray[indexPath.row] as! String)
-                yearViewObj.delegate = self
-                cell.commonTextFiled.inputView = yearViewObj
                 
-            case 2:
-                cell.commonTextFiled.placeholder = FieldType.OfficeName.description
             case 3:
-                cell.commonTextFiled.placeholder = FieldType.OfficeAddress.description
+                cell.commonTextFiled.placeholder = FieldType.YearOfExperience.description
+                let yearViewObj = ExperiencePickerView.loadExperiencePickerView(withText: self.experienceArray[indexPath.row-2] as! String)
+                yearViewObj.delegate = self
+                cell.commonTextFiled.tag = 1
+                cell.commonTextFiled.inputView = yearViewObj
+            case 4:
+                cell.commonTextFiled.placeholder = FieldType.OfficeName.description
+                cell.commonTextFiled.tag = 2
             default:
                 print("default")
             }
-
-            cell.commonTextFiled.text = self.experienceArray[indexPath.row] as? String
+            
+            cell.commonTextFiled.text = self.experienceArray[indexPath.row - 2] as? String
             cell.commonTextFiled.delegate = self
             return cell
 
+        default: break
         }
+
+        return UITableViewCell()
     }
     func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
         if let textField = textField as? AnimatedPHTextField {
