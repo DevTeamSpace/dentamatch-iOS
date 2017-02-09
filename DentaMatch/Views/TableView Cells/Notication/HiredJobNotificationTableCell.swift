2e65cf8c165cb91edac329c0cfa9c51070ed0539
Sanjay Kumar Yadav <sanjay.yadav@appster.com.au>
2017-02-09 12:14:12 +0530

    notification table cell created

---
+++ DentaMatch/Views/TableView Cells/Notication/HiredJobNotificationTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  HiredJobNotificationTableCell.swift
+//  DentaMatch
+//
+//  Created by Sanjay Kumar Yadav on 09/02/17.
+//  Copyright © 2017 Appster. All rights reserved.
+//
+
+import UIKit
+
+class HiredJobNotificationTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ DentaMatch/Views/TableView Cells/Notication/NotificationJobTypeTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  NotificationJobTypeTableCell.swift
+//  DentaMatch
+//
+//  Created by Sanjay Kumar Yadav on 09/02/17.
+//  Copyright © 2017 Appster. All rights reserved.
+//
+
+import UIKit
+
+class NotificationJobTypeTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
