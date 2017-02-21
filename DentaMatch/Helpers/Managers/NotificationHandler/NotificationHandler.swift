c51e52093f6b3212b847d22bf87e7bcbfa9ae4dc
Sanjay Kumar Yadav <sanjay.yadav@appster.com.au>
2017-02-21 12:22:51 +0530

    notification payload change

--- DentaMatch/Helpers/Managers/NotificationHandler/NotificationHandler.swift
+++ DentaMatch/Helpers/Managers/NotificationHandler/NotificationHandler.swift
@@ -46,48 +46,64 @@ class NotificationHandler: NSObject {
     }
     class func notificationHandleforBackground(notiObj:UserNotification, app : UIApplication) {
         let notificationType = UserNotificationType(rawValue: notiObj.notificationType!)!
-
+        
         switch notificationType {
         case .acceptJob:
-        //open job detail
-        openJobDetailScreen(obj: notiObj.jobdetail!)
-
+            //open job detail
+            openJobDetailScreenForBackGround(obj: notiObj.jobdetail!)
+            
         case .chatMessgae: break
         //No need any action
         case .completeProfile:
-        //open profile
-        openEditProfileScreen()
-
+            //open profile
+            openEditProfileScreenForBackground()
+            
         case .deleteJob: break
         //No need any action
         case .hired:
-        //open job detail
-            openJobDetailScreen(obj: notiObj.jobdetail!)
-
+            //open job detail
+            openJobDetailScreenForBackGround(obj: notiObj.jobdetail!)
+            
         case .jobCancellation:
-        //open job detail
-        openJobDetailScreen(obj: notiObj.jobdetail!)
-
+            //open job detail
+            openJobDetailScreenForBackGround(obj: notiObj.jobdetail!)
+            
         case .verifyDocuments:
-        //open edit profile
-            openEditProfileScreen()
+            //open edit profile
+            openEditProfileScreenForBackground()
         case .other: break
             //No need any action
             
         }
-
+        
         
     }
     
     class func openJobDetailScreen(obj:Job) {
         NotificationCenter.default.post(name: .pushRedirectNotificationForground, object: nil, userInfo: ["notificationData":obj])
-
-        
     }
     class func openEditProfileScreen() {
         NotificationCenter.default.post(name: .pushRedirectNotificationForProfile, object: nil, userInfo: nil)
+    }
+    
+    class func openJobDetailScreenForBackGround(obj:Job) {
+        delay(time: 3.0) { 
+            NotificationCenter.default.post(name: .pushRedirectNotificationForground, object: nil, userInfo: ["notificationData":obj])
 
-        
+        }
+    }
+    class func openEditProfileScreenForBackground() {
+        delay(time: 3.0) { 
+            NotificationCenter.default.post(name: .pushRedirectNotificationForProfile, object: nil, userInfo: nil)
+
+        }
+    }
+    
+    class func delay(time:TimeInterval,completionHandler: @escaping ()->()) {
+        let when = DispatchTime.now() + 0.01
+        DispatchQueue.main.asyncAfter(deadline: when) {
+            completionHandler()
+        }
     }
 
     
