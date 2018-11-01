a70d87272d3556c5fe1587e0f5df8dc92312d9a9
Prashant Gautam <prashant.gautam@appster.com.au>
2018-11-01 15:05:39 +0530

    state list implemented on initial edit profile

--- DentaMatch/Views/TableView Cells/Profile/AnimatedPHToolTipCell.swift
+++ DentaMatch/Views/TableView Cells/Profile/AnimatedPHToolTipCell.swift
@@ -12,9 +12,28 @@ class AnimatedPHToolTipCell: UITableViewCell {
     @IBOutlet weak var commonTextField: AnimatedPHTextField!
     @IBOutlet weak var accessoryLabel: UILabel!
     @IBOutlet weak var toolTipLabel: UILabel?
+    var showKeyboard: Bool = true
     
+    private var editingHandler: (String?) -> Void = { _ in }
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        commonTextField.delegate = self
+    }
+    
+    func editAction(_ handler: @escaping (String?) -> Void) {
+        editingHandler = handler
     }
 }
+
+extension AnimatedPHToolTipCell: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if !showKeyboard { textField.resignFirstResponder() }
+        guard let keyword = textField.text else {
+            editingHandler(nil)
+            return
+        }
+        editingHandler(keyword.trimText)
+    }
+}
+
