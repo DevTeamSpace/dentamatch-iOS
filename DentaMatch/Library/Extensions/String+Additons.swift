20433c9a3f875ec7a82ba183dc546fe9ed8c338f
k4russ <k4russ@gmail.com>
2019-01-24 09:24:04 +0200

    updates to repository

--- DentaMatch/Library/Extensions/String+Additons.swift
+++ DentaMatch/Library/Extensions/String+Additons.swift
@@ -64,7 +64,7 @@ extension String {
     subscript(r: Range<Int>) -> String {
         let start = index(startIndex, offsetBy: r.lowerBound)
         let end = index(start, offsetBy: r.upperBound)
-        return String(self[Range(start ..< end)])
+        return String(self[start..<end])
     }
 
     func lpad(_ padding: String, length: Int) -> String {
